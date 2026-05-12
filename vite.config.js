34bec2bd8fb83c123f6e5a0447f98efa7df62c59
Ricardo Cuc <rcuc@lucalza.com>
2026-05-12 13:39:13 -0600

    fix: corregir vercel.json para assets

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
   server: {
     host: true,
     port: 4173
   },
-  plugins: [react(), tailwindcss(), cloudflare()]
+  plugins: [react(), tailwindcss()]
 })
