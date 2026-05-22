d05e012cd4688f09faba1c947287a8443bd3d47c
ricardo-cuc <rh_cuc@hotmail.com>
2026-05-22 08:20:02 -0600

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { authService } from './authService';
 
-// DEBUG
-console.log('API URL:', import.meta.env.VITE_API);
+
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API,
